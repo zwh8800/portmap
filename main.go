0aa7496cdcb214c5130d69dd8d0ce7871ac3f3ee
Zhang Weihu <496781108@qq.com>
2017-03-09 17:11:25 +0800

    forward and clear

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/NebulousLabs/go-upnp"
 )
 
 func main() {
+	clear := flag.Bool("c", false, "clear")
+	port := flag.Int("p", 0, "port")
+
+	flag.Parse()
+
+	if *port <= 0 || *port > 65535 {
+		log.Panic("Port out of range")
+	}
+
 	igd, err := upnp.Discover()
 	if err != nil {
 		log.Panic("upnp.Discover():", err)
@@ -17,4 +27,14 @@ func main() {
 		log.Panic("igd.ExternalIP():", err)
 	}
 	log.Println("ExternalIP:", extIP)
+
+	if *clear {
+		if err := igd.Clear(uint16(*port)); err != nil {
+			log.Panic("igd.Forward():", err)
+		}
+	} else {
+		if err := igd.Forward(uint16(*port), "shadowsocks"); err != nil {
+			log.Panic("igd.Forward():", err)
+		}
+	}
 }
